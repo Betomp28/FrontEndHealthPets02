05a868c7fdba55742d65e10ea03278a88fc122df
Betomp28 <157768121+Betomp28@users.noreply.github.com>
2024-08-15 11:40:15 -0600

    Alberto (#1)
    * nueva rama
    
    * cambios uno
    
    * clase perfil lista
    
    * pirfil usuario listo
    
    * registro mascotas listo
    
    * registro y fotos listo

--- Entidades/Entitys/Sesion.cs
+++ Entidades/Entitys/Sesion.cs
@@ -8,9 +8,15 @@ namespace FrontEndHealthPets.Entidades.Entitys
 {
    public class Sesion
     {
+        public static long Id_mascota { get; set; }
         public static long id_usuario { get; set; }
         public static string nombre { get; set; }
         public static string apellidos { get; set; }
+
+        public static string Correo_Electronico { get; set; }
+
+        public static string Password { get; set; }
+
         public static string token { get; set; }
 
         public static bool validarSesion()
@@ -27,9 +33,12 @@ namespace FrontEndHealthPets.Entidades.Entitys
 
         public static void cerrarSesion()
         {
+            Sesion.Id_mascota = 0;
             Sesion.id_usuario = 0;
             Sesion.nombre = String.Empty;
             Sesion.apellidos = String.Empty;
+            Sesion.Correo_Electronico = String.Empty;
+            Sesion.Password = String.Empty;
             Sesion.token = String.Empty;
 
         }
