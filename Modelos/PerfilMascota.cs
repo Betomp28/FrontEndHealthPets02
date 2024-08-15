3c53cec2e29ecd19d1f10cee810d769bf2aab28d
MORERA PEREZ <alberto.morera@ulatina.net>
2024-08-15 16:59:32 -0600

    cambios nuevo perfilmascota

---
+++ Modelos/PerfilMascota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndHealthPets.Modelos
+{
+
+    public class PerfilMascota
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ImageSource ImageSource { get; set; }
+    }
+}
