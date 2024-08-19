cd8c874b9d28ff64de52664f9b58ae6775c33da6
MORERA PEREZ <alberto.morera@ulatina.net>
2024-08-19 00:50:30 -0600

    logica baheiroslista

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -21,9 +21,7 @@ namespace FrontEndHealthPets
 
         private async void btiniciarsecion_Clicked(object sender, EventArgs e)
         {
-           
-            
-         
+       
      try
     {
          // Validación de campos de inicio de sesión
@@ -87,8 +85,7 @@ namespace FrontEndHealthPets
              {
                  await DisplayAlert("Error de aplicación", "Reinstale la aplicación. Detalle: " + ex.Message, "Aceptar");
              }
-            
-            
+        
         }
 
 
