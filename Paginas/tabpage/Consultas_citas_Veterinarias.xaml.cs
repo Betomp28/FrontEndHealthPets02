b16bde8cbf979295e752fc3e74e8ca7394158118
Betomp28 <157768121+Betomp28@users.noreply.github.com>
2024-08-19 21:49:55 -0600

    Alberto02 (#14)
    * mediacmentos listo
    
    * medicamento listo
    
    * registromascota
    
    * clse registro medicamentos completa
    
    * todobien
    
    * mainarreglado
    
    * cambio pruieva
    
    * cambios agrego de tabbepage
    
    * logica baheiroslista
    
    * vacunas sirviendo
    
    * lista de medicamentos
    
    * consultas completas vacunas medicamentos baheiros

---
+++ Paginas/tabpage/Consultas_citas_Veterinarias.xaml.cs
@@ -0,0 +1,19 @@
+namespace FrontEndHealthPets.Paginas.tabpage;
+
+public partial class Consultas_citas_Veterinarias : ContentPage
+{
+	public Consultas_citas_Veterinarias()
+	{
+		InitializeComponent();
+	}
+
+    private void MascotaPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BtGuardarLista_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
