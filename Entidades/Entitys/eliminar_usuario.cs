4ab21604a57a3108e1cde368fb6e628e4429f74d
Betomp28 <157768121+Betomp28@users.noreply.github.com>
2024-08-21 11:19:43 -0600

    Alberto03 (#15)
    * mediacmentos listo
    
    * medicamento listo
    
    * registromascota
    
    * clse registro medicamentos completa
    
    * todobien
    
    * mainarreglado
    
    * cambio pruieva
    
    * cambios agrego de tabbepage
    
    * logica baheiroslista
    
    * vacunas sirviendo
    
    * lista de medicamentos
    
    * consultas completas vacunas medicamentos baheiros
    
    * canbio per
    
    * todo bien
    
    * cambios

---
+++ Entidades/Entitys/eliminar_usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndHealthPets.Entidades.entitys
+{
+    public class eliminar_usuario
+    {
+
+        public String correoElectronico { get; set; }
+
+    }
+}
---
+++ Entidades/Request/Req_EliminarUsuario.cs
@@ -0,0 +1,20 @@
+﻿using FrontEndHealthPets.Entidades.entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndHealthPets.Entidades
+{
+    public class Req_EliminarUsuario
+    {
+        public EliminarUsuarioDetails eliminar_usuario { get; set; }
+
+        // Clase anidada para detalles de eliminación de usuario
+        public class EliminarUsuarioDetails
+        {
+            public string correoElectronico { get; set; }
+        }
+    }
+}
