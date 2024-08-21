d569333cc96d359d5b87b8c9a37598585e076781
CeekPro <csr.moum@gmail.com>
2024-08-21 12:54:40 -0600

    se hace el front y parte de la lógica para mostar las mascotas

--- Modelos/PerfilMascota.cs
+++ Modelos/PerfilMascota.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 
 namespace FrontEndHealthPets.Modelos
 {
-
     public class PerfilMascota
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
         public ImageSource ImageSource { get; set; }
+        public string Especie { get; set; }
+        public string Raza { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
     }
 }
